aa8a01e1175b99a0c96b401abf300ea5bd582328
Laksh <lakshd1999@gmail.com>
2024-07-24 22:41:45 +0530

    Update cart_screen.dart

--- royal_plate/lib/cart_functionaltiy/cart_screen.dart
+++ royal_plate/lib/cart_functionaltiy/cart_screen.dart
@@ -29,83 +29,86 @@ class _CartScreenState extends State<CartScreen> {
         body: Column(
           children: [
             FutureBuilder(
-                // future: Provider.of<CartProvider>(context,listen: false).getData(),
-                builder: (context, AsyncSnapshot<List<Cart>> snapshot) {
-              if (snapshot.hasData) {
-                return Expanded(
-                    child: ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      Image.asset(
-                                        snapshot.data![index].image.toString(),
-                                        height: 120,
-                                        width: 140,
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Column(
-                                        children: [
-                                          Text(
-                                            snapshot.data![index].productname
-                                                .toString(),
-                                            style: TextStyle(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w500),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            snapshot.data![index].productprice
-                                                    .toString() +
-                                                " Rs ",
-                                            style: TextStyle(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w500),
-                                          ),
-                                          SizedBox(
-                                            height: 5,
-                                          ),
-                                          Align(
-                                            alignment: Alignment.centerRight,
-                                            child: InkWell(
-                                              onTap: () {},
-                                              child: Container(
-                                                height: 35,
-                                                width: 100,
-                                                decoration: BoxDecoration(
-                                                    color: Colors.green),
-                                                child: Center(
-                                                  child: Text(
-                                                    'Add to Cart',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
+              // future: Provider.of<CartProvider>(context,listen: false).getData(),
+              builder: (context, AsyncSnapshot<List<Cart>> snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                      child: ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Image.asset(
+                                          snapshot.data![index].image
+                                              .toString(),
+                                          height: 120,
+                                          width: 140,
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              snapshot.data![index].productname
+                                                  .toString(),
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              snapshot.data![index].productprice
+                                                      .toString() +
+                                                  " Rs ",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Align(
+                                              alignment: Alignment.centerRight,
+                                              child: InkWell(
+                                                onTap: () {},
+                                                child: Container(
+                                                  height: 35,
+                                                  width: 100,
+                                                  decoration: BoxDecoration(
+                                                      color: Colors.green),
+                                                  child: Center(
+                                                    child: Text(
+                                                      'Add to Cart',
+                                                      style: TextStyle(
+                                                          color: Colors.white),
+                                                    ),
                                                   ),
                                                 ),
                                               ),
-                                            ),
-                                          )
-                                        ],
-                                      )
-                                    ],
-                                  )
-                                ],
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        }));
-              }
-              return Text('');
-            }, future: null,)
+                            );
+                          }));
+                }
+                return Text('');
+              },
+              future: null,
+            )
           ],
         ));
   }
